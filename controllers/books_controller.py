72e4bdf2a41b45f863f0fd59a554dbbe119546b1
Mark Richardson <mark_richardson@sky.com>
2020-11-25 17:42:19 +0000

    View of all books set up on new route

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -3,11 +3,15 @@ from models.book import Book
 import repositories.book_repository as book_repository
 import repositories.author_repository as author_repository
 
-books_blueprint = Blueprint("tasks", __name__)
+books_blueprint = Blueprint("books", __name__)
 
 
 # INDEX
 # GET '/books'
+@books_blueprint.route("/books")
+def books():
+    books = book_repository.select_all()
+    return render_template("books/index.html", all_books = books)
 
 # NEW
 # GET '/books/new'
